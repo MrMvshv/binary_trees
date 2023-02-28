e988abdea0507dbc908b8cbf1cae27051a20db0d
MrMvshv <machariajosphat001@gmail.com>
2023-02-28 14:23:39 +0300

    4

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - checks if is leaf
+ * @node: node to check
+ *
+ * Return: 1 if leaf, 0 otherwise
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	if (node->right || node->left)
+		return (0);
+	return 1;
+}
