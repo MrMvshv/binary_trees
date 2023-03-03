69b855815180854cf63339306afcaf5b321e8542
MrMvshv <machariajosphat001@gmail.com>
2023-03-03 21:10:08 +0300

    102

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -2,28 +2,38 @@
 #include "binary_trees.h"
 
 int is_completed(binary_tree_t *tree);
+/**
+ * count_nodes - count nodes
+ * @tree: tree
+ *
+ * Return: nodes
+ */
 int count_nodes(binary_tree_t *tree)
 {
-    if (tree == NULL) {
-        return 0;
-    }
-    return 1 + count_nodes(tree->left) + count_nodes(tree->right);
+	if (tree == NULL)
+		return (0);
+	return (1 + count_nodes(tree->left) + count_nodes(tree->right));
 }
-/* Helper function to check if the tree is complete */
+/**
+ * is_complete - check if the tree is complete
+ * @tree: tree
+ * @index: index
+ * @node_count: nodes
+ *
+ * Return: true or false
+ */
 bool is_complete(binary_tree_t *tree, int index, int node_count)
 {
-    /* If tree is empty, return true */
-    if (tree == NULL) {
-        return true;
-    }
+	if (tree == NULL)
+		return (true);
 
-    /* If index is greater than or equal to the number of nodes in the tree, return false */
-    if (index >= node_count) {
-        return false;
-    }
+	if (index >= node_count)
+		return (false);
 
     /* Recursively check if left and right subtrees are complete */
-    return is_complete(tree->left, 2 * index + 1, node_count) && is_complete(tree->right, 2 * index + 2, node_count);
+	return (is_complete(tree->left,
+2 * index + 1, node_count) && is_complete(tree->right,
+2 * index + 2, node_count));
 }
 /**
  * binary_tree_is_complete - measures completeness
@@ -39,7 +49,7 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 	return (is_completed((binary_tree_t *)tree));
 }
 /**
- * is_complete - measures completeness
+ * is_completed - measures completeness
  * @tree: tree
  *
  * Return: 1 if full
@@ -47,9 +57,9 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 int is_completed(binary_tree_t *tree)
 {
 	int node_count, index;
+
 	node_count = count_nodes((binary_tree_t *)tree);
 	index = 0;
-
 	if (is_complete((binary_tree_t *)tree, index, node_count))
 		return (1);
 	return (0);
